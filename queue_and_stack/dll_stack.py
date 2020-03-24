fb40a439da1938cb250c740cd6d6874bdea88807
Mark Halls <mark.halls@gmail.com>
2020-03-24 09:18:14 -0700

    Refactor using dll size

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,21 +6,17 @@ from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
-        self.size = 0
         # Why is our DLL a good choice to store our elements?
         # because we need to keep track of the front and back of the queue
-
         self.storage = DoublyLinkedList()
 
     def push(self, value):
-        self.size += 1
         self.storage.add_to_tail(value)
 
     def pop(self):
-        if self.size < 1:
+        if self.len() < 1:
             return
-        self.size -= 1
         return self.storage.remove_from_tail()
 
     def len(self):
-        return self.size
+        return len(self.storage)
