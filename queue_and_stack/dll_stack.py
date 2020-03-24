398830acd7ec6ea5aa404d6df9355156b0c6a4ee
Mark Halls <mark.halls@gmail.com>
2020-03-23 21:06:23 -0700

    Can push, pop and return length of the stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -1,18 +1,26 @@
 import sys
-sys.path.append('../doubly_linked_list')
+
+sys.path.append("../doubly_linked_list")
 from doubly_linked_list import DoublyLinkedList
+
 
 class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # because we need to keep track of the front and back of the queue
+
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.size += 1
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        if self.size < 1:
+            return
+        self.size -= 1
+        return self.storage.remove_from_tail()
 
     def len(self):
-        pass
+        return self.size
